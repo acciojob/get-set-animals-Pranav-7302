3ab8dbb4cfec5b43b16af8c96462164d767444a7
Pranav Gupta <146617022+Pranav-7302@users.noreply.github.com>
2025-03-08 15:02:37 +0530

    question resolved

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 //complete this code
-class Animal {}
+class Animal {
+    constructor(species){
+        this.species = species;
+    }
+    get Species(){
+        return this.species;
+    }
+    makesound(){
+        console.log(`The ${this.species} makes a sound`);
+    }
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+    bark(){
+        console.log(`woof`);
+    }
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+    purr(){
+        console.log(`purr`);
+    }
+}
 
 // Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+// window.Animal = Animal;
+// window.Dog = Dog;
+// window.Cat = Cat;
+
+const myCat = new Cat('Siamese');
+myCat.makesound();
+myCat.purr();
+
+const myDog = new Dog('Golden Retriever');
+myDog.makesound();
+myDog.bark();
